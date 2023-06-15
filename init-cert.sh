efa4289395f6832f3af54d773717ca110d157246
Kanta Oikawa <k.oikawa@kantacky.com>
2023-06-15 22:04:21 +0000

    [Update] Update init-cert.sh

--- init-cert.sh
+++ init-cert.sh
@@ -1,6 +1,8 @@
 docker-compose down
 docker-compose -f docker-compose.http.yml up -d nginx
-docker-compose -f docker-compose.http.yml run --rm certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email hq@kantacky.com --agree-tos -d www.kantacky.com -d www.chatlab.space -d www.nanosuke.net
+docker-compose -f docker-compose.http.yml run --rm certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email hq@kantacky.com --agree-tos -d www.kantacky.com
+docker-compose -f docker-compose.http.yml run --rm certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email hq@kantacky.com --agree-tos -d www.chatlab.space
+docker-compose -f docker-compose.http.yml run --rm certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email hq@kantacky.com --agree-tos -d www.nanosuke.net
 docker-compose -f docker-compose.http.yml down
 docker-compose up -d
 docker-compose logs -f
